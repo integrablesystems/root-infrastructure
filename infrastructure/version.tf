e1f5b3021365c818de7c8a570456854a633fe1c9
Integrable Systems <opensource@integrable.systems>
2021-08-15 19:05:57 +0800

    Update Terraform and providers

--- infrastructure/version.tf
+++ infrastructure/version.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.23"
+      version = "~> 3.54"
     }
     github = {
       source  = "integrations/github"
-      version = "~> 4.2"
+      version = "~> 4.13"
     }
   }
-  required_version = ">= 0.14"
+  required_version = ">= 1.0"
 
   backend "s3" {
     bucket         = "integrable-systems-terraform-state"
